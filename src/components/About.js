efe0bc0ad74fe278abdbf49eda745a71e6aa19f2
mohammed <mohammed.salad@student.moringaschool.com>
2023-11-20 18:13:55 +0300

    done

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,15 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">
+    <h2>About Me</h2>
+    <p>I am one of the most intellectual web devs in the world stop playing with me bruv!</p>
+    <img src ={image} alt="I made this"></img>
+
+  </div>
+  
+  );
 }
 
 export default About;
